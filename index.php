a637935a41b460bc54d065a8c1ac1fb92a732a39
Bratok2202 <piskabobra12sm@gmail.com>
2024-12-11 12:07:57 +0700

    e

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 $pdo = require $_SERVER['DOCUMENT_ROOT'] . '/db.php';
-$prodq = $pdo->prepare('select * from products');
+$prodq = $pdo->prepare('SELECT products.*, sum(uchet_tovarov.amount) as amount from products left join uchet_tovarov on uchet_tovarov.product_id = products.id group by products.id');
 $prodq->execute();
 $prod = $prodq->fetchAll(PDO::FETCH_ASSOC);
+var_dump($prod);
+foreach ($prod as $pro) {
+    if ($pro['amount'] == null) {
+        $pro['amount'] = 0;
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,6 +26,7 @@ $prod = $prodq->fetchAll(PDO::FETCH_ASSOC);
             <td>Name</td>
             <td>Cost</td>
             <td>Article</td>
+            <td>amount</td>
             <td><a href="/create.php" id="create">Add new product</a></td>
             <td><a href="/postup.php" id="postup">Add arrive of product</a></td>
         </tr>
@@ -28,6 +35,7 @@ $prod = $prodq->fetchAll(PDO::FETCH_ASSOC);
                 <td><?= $pr['name'] ?></td>
                 <td><?= $pr['how_much'] ?></td>
                 <td><?= $pr['id'] ?></td>
+                <td><?= $pr['amount'] ?></td>
             </tr>
         <? endforeach ?>
     </table>
